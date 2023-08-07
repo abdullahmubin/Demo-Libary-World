a927fa6890bf4bdcbd1c0898587e704ef7a4ffa3
mubinCodemen <codemen.almubin@gmail.com>
2023-08-07 19:52:51 +0600

    add-migration InitialDatabaseCreation executed.

---
+++ DemoLibWorld/Migrations/20230807135152_InitialDatabaseCreation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoLibWorld.Migrations
+{
+    public partial class InitialDatabaseCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BoookCollectoins",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Authorname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Releasedate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Rating = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoookCollectoins", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BoookCollectoins");
+        }
+    }
+}
