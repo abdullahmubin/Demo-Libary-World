e6138115e838148bced1798620266f28b9e1841c
mubinCodemen <codemen.almubin@gmail.com>
2023-08-10 22:36:35 +0600

    sort and other solved.

--- DemoLibWorld/Pages/Books/Index.cshtml.cs
+++ DemoLibWorld/Pages/Books/Index.cshtml.cs
@@ -13,6 +13,12 @@ namespace DemoLibWorld.Pages.Books
     public class IndexModel : PageModel
     {
         private readonly DemoLibWorld.DataLayer.BookDBContext _context;
+         
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public string BookCategorySort { get; set; }
+        public string DateSort { get; set; }
+        public string RatingSort { get; set; }
 
         public IndexModel(DemoLibWorld.DataLayer.BookDBContext context)
         {
@@ -21,16 +27,43 @@ namespace DemoLibWorld.Pages.Books
 
         public IList<BookEntity> BookEntity { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
             if (_context.BoookCollectoins != null)
             {
+                TitleSort = sortOrder == "title_asc_sort" ? "title_desc_sort" : "title_asc_sort";
+                AuthorSort = sortOrder == "author_asc_sort" ? "author_desc_sort" : "author_asc_sort";
+                BookCategorySort = sortOrder == "bookcategory_asc_sort" ? "bookcategory_desc_sort" : "bookcategory_asc_sort";
+                RatingSort = sortOrder == "rating_asc_sort" ? "rating_desc_sort" : "rating_asc_sort";
+                DateSort = sortOrder == "Date" ? "date_desc_" : "Date";
+
+
                 BookEntity = await _context.BoookCollectoins.ToListAsync();
 
                 foreach (var item in BookEntity)
                 {
                     item.BookCategories = await _context.BookCategories.FirstOrDefaultAsync(x
                         => x.BookCategoryId == item.BookCategoryId);
+                }
+
+                switch(sortOrder)
+                {
+                    case "title_asc_sort":
+                        BookEntity = BookEntity.OrderBy(s => s.Title).ToList();
+                        break;
+                    case "title_desc_sort":
+                        BookEntity = BookEntity.OrderByDescending(s => s.Title).ToList();
+                        break;
+                    case "author_asc_sort":
+                        BookEntity = BookEntity.OrderBy(s => s.Authorname).ToList();
+                        break;
+                    case "author_desc_sort":
+                        BookEntity = BookEntity.OrderByDescending(s => s.Authorname).ToList();
+                        break;
+
+                    default:
+                        BookEntity = BookEntity.OrderBy(s => s.Title).ToList();
+                        break;
                 }
             }
         }
